54e3b0231ca3f87728eeb85084fc7324e40ffcc1
Rus0d <rusya.liamg@gmail.com>
2016-08-26 18:59:06 +0300

    3-rd  block begin

--- dist/script/swiperinit.js
+++ dist/script/swiperinit.js
@@ -1,13 +1,28 @@
 $(document).ready(function(){	
 
-	var swiper = new Swiper('.swiper-container', {
-        pagination: '.swiper-pagination',
+	var showroomSlider = new Swiper('.showroom .swiper-container', {
         nextButton: '.swiper-button-next',
         prevButton: '.swiper-button-prev',
-        slidesPerView: 1,
+        slidesPerView: 3,
+        slidesPerColumn: 2,
         paginationClickable: true,
-        spaceBetween: 30,
-        loop: true
+        spaceBetween: 80,
+        loop: true,
+        centeredSlides: true,
+        autoplay: 15000,
+        autoplayDisableOnInteraction: false,
+        breakpoints: {
+            // when window width is <= 500px
+            500: {
+                slidesPerView: 1,
+                centeredSlides: true
+            },
+            // when window width is <= 768px
+            768: {
+                slidesPerView: 2,
+                centeredSlides: false
+            }
+        }
     });
 
 });
